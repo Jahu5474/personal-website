cac7844d98a1ff9e65c845ca5b14a9a0a7c79642
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-04-08 23:35:56 -0700

    added divider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Heading, Text, Stack, Container, Button, Box } from '@chakra-ui/react'
+import { Heading, Text, Stack, Container, Button, Box, Divider } from '@chakra-ui/react'
 import { NextPage } from 'next';
 import style from '../styles/app.module.css'
 
@@ -32,6 +32,7 @@ const Home: NextPage = () => {
           >
             I always enjoyed creating and using the creative side of me and through a friend I discovered the world of programming. As a child, technology have always peaked my interest and would always play around with them. I gained experience in coding by doing a web development online bootcamp in the summer of 2022. I am located in Vancouver, BC, Canada, where I will continue to grind hard and create things out of thin air with codes.
           </Text>
+          <Divider pb={4} />
         </Container>
       </Box>
     </>
