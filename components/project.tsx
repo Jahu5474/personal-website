907669861768cf6012353cac0e6697ce10c2ef5e
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-04-10 17:08:30 -0700

    trying to add git repo to app

--- components/project.tsx
+++ components/project.tsx
@@ -1,45 +1,13 @@
-import React from 'react';
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
-import { listUserReposResponse } from "@/hooks/useProjects";
+// import { Box, Heading, Link, Text } from '@chakra-ui/react';
+// import { motion } from 'framer-motion';
 
+// const Repositories = ({ repositories }) => (
+    
+// );
 
-const project = () => {
-    // const [selectedId, setSelectedId] = useState(null)
-    const [repositories, setRepositories] = useState([]);
+// export async function getStaticProps() {
+//     const repositories = await fetchRepositories();
+//     return { props: { repositories } };
+// }
 
-    useEffect(() => {
-        const fetchRepositories = async () => {
-            const response = await listUserReposResponse(process.env.GITHUB_TOKEN);
-            setRepositories(response.data);
-        };
-        fetchRepositories();
-    }, []);
-
-    // Rest of the component code
-}
-return (
-    <>
-
-
-
-        {/* {projects.map(item => (
-                <motion.div layoutId={item.id} onClick={() => setSelectedId(item.id)}>
-                    <motion.h5>{item.subtitle}</motion.h5>
-                    <motion.h2>{item.title}</motion.h2>
-                </motion.div>
-            ))}
-
-            <AnimatePresence>
-                {selectedId && (
-                    <motion.div layoutId={selectedId}>
-                        <motion.h5>{item.subtitle}</motion.h5>
-                        <motion.h2>{item.title}</motion.h2>
-                        <motion.button onClick={() => setSelectedId(null)} />
-                    </motion.div>
-                )}
-            </AnimatePresence> */}
-    </>
-)
-}
+// export default Repositories;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { Heading, Text, Stack, Container, Button, Box, Divider, Flex } from '@chakra-ui/react'
 import { NextPage } from 'next';
 import style from '../styles/app.module.css'
+import Repositories from '@/hooks/useProjects';
 
 const Home: NextPage = () => {
   return (
@@ -22,7 +23,7 @@ const Home: NextPage = () => {
                 I always enjoyed creating and using the creative side of me and through a friend I discovered the world of programming. As a child, technology have always peaked my interest and would always play around with them. I gained experience in coding by doing a web development online bootcamp in the summer of 2022. I am located in Vancouver, BC, Canada, where I will continue to grind hard and create things out of thin air with codes.
               </Text>
             </Flex>
-            {/* <project/> */}
+            <Repositories repositories={Repositories} />
 
           </Flex>
         </Flex>
