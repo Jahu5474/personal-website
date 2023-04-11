6586fedd068f203417b4cedc6dd96332143a4ad1
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-04-10 17:45:47 -0700

    trying to implement git repos

--- components/project.tsx
+++ components/project.tsx
@@ -1,13 +1,35 @@
-// import { Box, Heading, Link, Text } from '@chakra-ui/react';
-// import { motion } from 'framer-motion';
+import { GetStaticProps } from "next";
+import Repositories from "../components/Repositories";
 
-// const Repositories = ({ repositories }) => (
-    
-// );
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
 
-// export async function getStaticProps() {
-//     const repositories = await fetchRepositories();
-//     return { props: { repositories } };
-// }
+interface Props {
+    repositories: Repository[];
+}
 
-// export default Repositories;
+export const getStaticProps: GetStaticProps = async () => {
+    const response = await fetch(`https://api.github.com/users/Jahu5474/repos?per_page=100`);
+    const repositories = await response.json();
+
+    return {
+        props: {
+            repositories,
+        },
+    };
+};
+
+const Home = ({ repositories }: Props) => {
+    return (
+        <div>
+            <Repositories repositories={repositories} />
+        </div>
+    );
+};
+
+
+
+export default Home;
