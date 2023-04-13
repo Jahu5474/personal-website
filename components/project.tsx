429577c7e78f786daa4b6745379751ec3fe6ed79
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-04-13 14:48:45 -0700

    deleted a bunch of file i tried doing the github repo thing but didnt work

--- components/project.tsx
+++ components/project.tsx
@@ -1,53 +1,17 @@
-import { useEffect, useState } from 'react';
-import { Flex, Text } from '@chakra-ui/react';
-import { getRepoInfo } from '../components/github'
-
-const useFetchRepoData = (username: string, repoName: string, token: string) => {
-    const [repoData, setRepoData] = useState<any>(null);
-
-    useEffect(() => {
-        const fetchRepoData = async () => {
-            try {
-                const response = await fetch(`https://api.github.com/repos/${username}/${repoName}`, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                });
-                const data = await response.json();
-                setRepoData(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchRepoData();
-    }, [username, repoName, token]);
-
-    return repoData;
-};
+import { Heading, Flex } from '@chakra-ui/react';
+import React from 'react';
 
 const Project = () => {
-    const username: string = 'Jahu5474';
-    const repoName: string = 'Creative-Eyecare-Centre';
-    const token: any = process.env.GITHUB_ACCESS_TOKEN;
-
-    const repoData = useFetchRepoData(username, repoName, token);
-
-    if (!repoData) {
-        return (
-            <Flex justify="center" align="center" height="100vh">
-                <Text>Loading...</Text>
-            </Flex>
-        );
-    }
-
     return (
-        <Flex direction="column" align="center">
-            <Text fontSize="xl" fontWeight="bold">{repoData.full_name}</Text>
-            <Text fontSize="md">{repoData.description}</Text>
-            <Text fontSize="md">Language: {repoData.language}</Text>
-            <Text fontSize="md">Stars: {repoData.stargazers_count}</Text>
-        </Flex>
-    );
-};
+        <>
+            <Flex>
+                <Heading>
+                    My Projects
+                </Heading>
+
+            </Flex>
+        </>
+    )
+}
 
 export default Project;
