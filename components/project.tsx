7dc6209dd801b207a33426afb5293ede92c5aa93
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-05-15 00:25:21 +0900

    added cards for project display

--- components/project.tsx
+++ components/project.tsx
@@ -1,11 +1,57 @@
-import { Heading, Flex, Text } from '@chakra-ui/react';
+import { Heading, Flex, Text, Card, SimpleGrid, CardHeader, CardBody, Button, CardFooter } from '@chakra-ui/react';
 import React from 'react';
 
 const Project = () => {
+    const Size = 'md'
+
     return (
         <>
-            <Flex>
-                <Heading>Projects</Heading>
+            <Heading
+                mt={4}
+                mb={4}
+            >
+                Projects
+            </Heading>
+            <Flex
+                direction={{ base: "row", md: "column" }}
+
+            >
+
+                <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(250px, 1fr))' height="300px">
+                    <Card>
+                        <CardHeader>
+                            <Heading size={Size}> Customer dashboard</Heading>
+                        </CardHeader>
+                        <CardBody>
+                            <Text>View a summary of all your customers over the last month.</Text>
+                        </CardBody>
+                        <CardFooter>
+                            <Button>View here</Button>
+                        </CardFooter>
+                    </Card>
+                    <Card>
+                        <CardHeader>
+                            <Heading size={Size}> Customer dashboard</Heading>
+                        </CardHeader>
+                        <CardBody>
+                            <Text>View a summary of all your customers over the last month.</Text>
+                        </CardBody>
+                        <CardFooter>
+                            <Button>View here</Button>
+                        </CardFooter>
+                    </Card>
+                    <Card>
+                        <CardHeader>
+                            <Heading size={Size}> Customer dashboard</Heading>
+                        </CardHeader>
+                        <CardBody>
+                            <Text>View a summary of all your customers over the last month.</Text>
+                        </CardBody>
+                        <CardFooter>
+                            <Button>View here</Button>
+                        </CardFooter>
+                    </Card>
+                </SimpleGrid>
 
 
             </Flex>
